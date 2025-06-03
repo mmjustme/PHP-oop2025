57c782d6a07e20198620ed720d20f75362d8de1e
mmjustme <infooneuser@gmail.com>
2025-06-03 21:10:15 +0300

    (les3): dynemic parameters

--- 03/index.php
+++ 03/index.php
@@ -1,6 +1,25 @@
 <?php
 
+error_reporting(-1);
+
 require_once __DIR__ . "/Product.php";
 
-$product = new Product();
-var_dump($product);
+$book = new Product();
+$book->title = "Book";
+$book->price = 20.1;
+var_dump($book);
+var_dump($book->price);
+
+
+echo "<br>";
+
+$phone = new Product();
+$phone->title = "Phone";
+$phone->price = 39;
+
+// Динамічний параметр
+//помилкове значення читається як новий параметр
+// і відповідно буде стоврений як новий параметр
+$phone->tite = "Phone";
+var_dump($phone);
+var_dump($phone->title);
