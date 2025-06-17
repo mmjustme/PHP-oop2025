5a165533190fa90e79e837612f1f5e3ced8cc495
mmjustme <infooneuser@gmail.com>
2025-06-17 22:02:20 +0300

    (les6):(updated) Magic - construct

--- 06/Product.php
+++ 06/Product.php
@@ -2,21 +2,31 @@
 
 class Product
 {
-    public ?string $title;
-    public int $price;
+//    public ?string $title;
+//    public int $price;
+//
+//    public function __construct(
+//
+//// якщо потрібно, щоб користувач обовязково передав дані
+//// забираємо значення за замовчуванням
+////  string $title = "Some Product",
+////  int $price = 0
+//        string $title,
+//        int    $price
+//    )
+//    {
+//        $this->title = $title;
+//        $this->price = $price;
+//    }
 
+//спрощений запис одразу ініціалізація і привоєння
     public function __construct(
-
-// якщо потрібно, щоб користувач обовязково передав дані
-// забираємо значення за замовчуванням
-//  string $title = "Some Product",
-//  int $price = 0
-        string $title,
-        int    $price
+        public string $title = "Some Product",
+        public int    $price,
     )
     {
-        $this->title = $title;
-        $this->price = $price;
+//        тіло методу не потребує шаблонного коду
+//        $this->title = $title;
     }
 
     public function getRealPrice($currency = "$"): string
